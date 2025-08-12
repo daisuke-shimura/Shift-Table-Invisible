bec05a65dc55ff34f2b4da9dfcc1f7e5ab32983a
daisuke-shimura <daisuke.s0707@icloud.com>
2025-08-13 01:01:44 +0900

    Bootstrap導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "stylesheets/application.scss"
+import "bootstrap"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  root to: 'public/homes#top'
+
   devise_for :admins
   devise_for :users
+
+  namespace :admin do
+
+  end
+
+  scope module: :public do
+    
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
