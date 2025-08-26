41c79e0d601700f367f403a090596f9fcd2e3ce4
daisuke-shimura <daisuke.s0707@icloud.com>
2025-08-27 01:13:01 +0900

    祝日判定 導入

--- app/helpers/admin/weeks_helper.rb
+++ app/helpers/admin/weeks_helper.rb
@@ -1,11 +1,2 @@
 module Admin::WeeksHelper
-
-  def holiday_class(date)
-    if Holidays.on(date, :jp).any?
-      return 'text-danger'
-    else
-      return nil
-    end
-  end
-
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def holiday_class(date)
+    if Holidays.on(date, :jp).any?
+      return 'text-danger'
+    else
+      return nil
+    end
+  end
 end
